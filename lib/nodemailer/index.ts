f77f164f46944c0c3c9711a568b053ac5f0b131e
GoodBoyBadVibes <eekwebene@gmail.com>
2025-10-31 15:53:04 +0100

    rebase commit that contained secret

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,15 +1,63 @@
 import nodemailer from 'nodemailer';
+import {google} from 'googleapis';
 import {WELCOME_EMAIL_TEMPLATE, NEWS_SUMMARY_EMAIL_TEMPLATE} from "@/lib/nodemailer/templates";
 
-export const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.NODEMAILER_EMAIL!,
-        pass: process.env.NODEMAILER_PASSWORD!,
-    }
-})
+// export const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: process.env.NODEMAILER_EMAIL!,
+//         pass: process.env.NODEMAILER_PASSWORD!,
+//     }
+// })
+// export const transporter = nodemailer.createTransport({
+//     host: 'smtp.gmail.com',
+//     port: 465,
+//     secure: true,
+//     auth: {
+//         type: 'OAuth2',
+//         user: 'user@example.com',
+//         clientId: process.env.CLIENT_ID!,
+//         clientSecret: process.env.CLIENT_SECRET!,
+//         refreshToken: process.env.REFRESH_TOKEN!,
+//         accessToken: await OAuth2Client.getAccessToken().then(res => res.token as string)
+//     }
+// });
+
+const OAuth2Client = new google.auth.OAuth2(
+    process.env.CLIENT_ID!,
+    process.env.CLIENT_SECRET!,
+    "https://developers.google.com/oauthplayground"
+);
+
+
+
+// export const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         type: 'OAuth2',
+//         user: process.env.NODEMAILER_EMAIL!,
+//         clientId: process.env.CLIENT_ID!,
+//         clientSecret: process.env.CLIENT_SECRET!,
+//         refreshToken: process.env.REFRESH_TOKEN!,
+//         accessToken: await OAuth2Client.getAccessToken().then(res => res.token as string)
+//     }
+// });
 
 export const sendWelcomeEmail = async ({ email, name, intro }: WelcomeEmailData) => {
+
+    const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: process.env.NODEMAILER_EMAIL!,
+        clientId: process.env.CLIENT_ID!,
+        clientSecret: process.env.CLIENT_SECRET!,
+        refreshToken: process.env.REFRESH_TOKEN!,
+        accessToken: await OAuth2Client.getAccessToken().then(res => res.token as string)
+    }
+});
+
+    
     const htmlTemplate = WELCOME_EMAIL_TEMPLATE
         .replace('{{name}}', name)
         .replace('{{intro}}', intro);
@@ -28,6 +76,19 @@ export const sendWelcomeEmail = async ({ email, name, intro }: WelcomeEmailData)
 export const sendNewsSummaryEmail = async (
     { email, date, newsContent }: { email: string; date: string; newsContent: string }
 ): Promise<void> => {
+
+    const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: process.env.NODEMAILER_EMAIL!,
+        clientId: process.env.CLIENT_ID!,
+        clientSecret: process.env.CLIENT_SECRET!,
+        refreshToken: process.env.REFRESH_TOKEN!,
+        accessToken: await OAuth2Client.getAccessToken().then(res => res.token as string)
+    }
+});
+
     const htmlTemplate = NEWS_SUMMARY_EMAIL_TEMPLATE
         .replace('{{date}}', date)
         .replace('{{newsContent}}', newsContent);
--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,11 @@ const nextConfig: NextConfig = {
       ignoreDuringBuilds: true,
   }, typescript: {
       ignoreBuildErrors: true
+<<<<<<< HEAD
     }
+=======
+  }
+>>>>>>> 3c83a32 (ignore eslint and typescript during builds)
 };
 
 export default nextConfig;
