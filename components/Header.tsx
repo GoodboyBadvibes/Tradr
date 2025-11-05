8e13e9d7c5f7bdad200f67bfc2709511575c2ad8
GoodBoyBadVibes <eekwebene@gmail.com>
2025-11-05 10:38:31 +0100

    => Tradr

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,10 +8,10 @@ const Header = async ({ user }: { user: User }) => {
     const initialStocks = await searchStocks();
 
     return (
-        <header className="sticky top-0 header">
+        <header className="sticky top-0 header bg-[#0A1747]/80 backdrop-blur-md">
             <div className="container header-wrapper">
                 <Link href="/">
-                    <Image src="/assets/icons/logo.svg" alt="Signalist logo" width={140} height={32} className="h-8 w-auto cursor-pointer" />
+                    <Image src="/assets/images/logo.svg" alt="Tradr logo" width={140} height={32} className="h-8 w-auto cursor-pointer" />
                 </Link>
                 <nav className="hidden sm:block">
                     <NavItems initialStocks={initialStocks} />
--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -68,10 +68,10 @@ export const sendWelcomeEmail = async ({ email, name, intro }: WelcomeEmailData)
         .replace('{{intro}}', intro);
 
     const mailOptions = {
-        from: `"Signalist" <signalist@jsmastery.pro>`,
+        from: `"Tradr" <eekwebene@gmail.com>`,
         to: email,
-        subject: `Welcome to Signalist - your stock market toolkit is ready!`,
-        text: 'Thanks for joining Signalist',
+        subject: `Welcome to Tradr - your stock market toolkit is ready!`,
+        text: 'Thanks for joining Tradr',
         html: htmlTemplate,
     }
 
@@ -99,10 +99,10 @@ export const sendNewsSummaryEmail = async (
         .replace('{{newsContent}}', newsContent);
 
     const mailOptions = {
-        from: `"Signalist News" <signalist@jsmastery.pro>`,
+        from: `"Tradr News" <eekwebene@gmail.com>`,
         to: email,
         subject: `📈 Market News Summary Today - ${date}`,
-        text: `Today's market news summary from Signalist`,
+        text: `Today's market news summary from Tradr`,
         html: htmlTemplate,
     };
 
